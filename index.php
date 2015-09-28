ae62bc78707375f34470ee29a632fdf15b3bdbeb
papangping <papangping@gmail.com>
2015-09-28 10:11:39 +0700

    init

---
+++ index.php
@@ -0,0 +1,278 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Tufftex Media</title>
+    <link rel="stylesheet" href="asset/tufftex-media/css/fonts.css">
+    <link rel="stylesheet" href="asset/tufftex-media/css/style.css">
+
+    <script type="text/javascript" src="asset/jquery/jquery-1.11.3.min.js"></script>
+    <script type="text/javascript" src="asset/jquery/jquery-1.11.3.min.map"></script>
+</head>
+<body>
+<div id="page-container">
+    <div class="section" id="about">
+        <div class="section-header">
+            <div class="spacial-title">
+                <div class="spacial-title-bg"></div>
+                <div class="spacial-title-text"></div>
+            </div>
+            <div class="section-description">
+                Yes!!
+            </div>
+        </div>
+    </div>
+    <div class="section" id="media">
+        <div class="section-header">
+            <div class="spacial-title">
+                <div class="spacial-title-bg"></div>
+                <div class="spacial-title-text"></div>
+            </div>
+            <div class="section-description">
+                To raise new questions, new possibilities, to
+                regard old problems from a new angle,
+                requires creative imagination and marks real
+                advance in science.
+            </div>
+        </div>
+
+        <div class="section-container">
+            <div class="container">
+                <div class="section-title">
+                    <h2>Our Media</h2>
+                </div>
+                <div class="media-item-container">
+                    <div class="media-item first-item">
+                        <div class="media-title">Digital wifi ads</div>
+                        <div class="media-description">Taxi goes where people go,
+                            where people work,
+                            where people live,shop and play</div>
+                        <div class="view-more-btn noselect">VIEW MORE</div>
+                    </div>
+                    <div class="media-item">
+                        <div class="media-title">LED Lighting</div>
+                        <div class="media-description">Taxi goes where people go,
+                            where people work,
+                            where people live,shop and play</div>
+                        <div class="view-more-btn noselect">VIEW MORE</div>
+                    </div>
+                    <div class="media-item">
+                        <div class="media-title">Digital Billboard</div>
+                        <div class="media-description">Taxi goes where people go,
+                            where people work,
+                            where people live,shop and play</div>
+                        <div class="view-more-btn noselect">VIEW MORE</div>
+                    </div>
+                </div>
+            </div>
+            <div class="lest-talk-container">
+                <div class="lest-talk-title">Have a project in mind ?</div>
+                <div class="lest-talk-btn show-contact-btn">let’s Talk</div>
+            </div>
+        </div>
+    </div>
+    <div class="section" id="investor">
+        <div class="section-header">
+            <div class="spacial-title">
+                <div class="spacial-title-bg"></div>
+                <div class="spacial-title-text"></div>
+            </div>
+            <div class="section-description">
+                Investment is time, energy, or matter spent
+                in the hope of future benefits actualized
+                within a specified date or time frame.
+                Investment has a different meaning in finance
+                from that in economics.
+            </div>
+        </div>
+        <div class="section-container">
+            <div class="parallax parallax1">
+                <div class="parallax-bg"></div>
+                <div class="parallax-description">
+                    <span style="font-size: 37px; color: #229ea1;">Tufftex media</span> is one of the most diversified<br class="hide-mobile">
+                    and innovative Out - of Home media providers in Thailand.</div>
+            </div>
+            <div class="divider"></div>
+            <div class="detail1">
+                ปัจจุบันผู้ออกแบบคิดค้น และพัฒนาระบบป้ายโฆษณาบนแท็กซี่จากเทคโนโลยีนี้<br class="hide-mobile">
+                ต้องการผู้ร่วมหุ้นร่วมลงทุน เพื่อร่วมกันพัฒนาป้ายโฆษณาบนแท็กซี่ิเทคโนโลยีใหม่นี้<br class="hide-mobile">
+                ให้ประสบความสำเร็จ เพื่อให้สามารถเข้าสู่ตลาดพลังงานโลกในเชิงพาณิชย์ได้<br>
+                <br>
+                เจ้าของเทคโนโลยีจึงขอเชิญชวนให้ผู้ที่สนใจและต้องการสนับสนุนเพื่อความสำเร็จในเชิงพาณิชย์<br class="hide-mobile">
+                ได้เข้ามาร่วมลงทุนกับเจ้าของเทคโนโลยีนี้<br>
+                <br>
+                รายละเอียดเพิ่มเติม<br>
+                <a href="" class="lest-talk-btn" style="margin-top: 20px; font-size: 30px;">Download</a>
+            </div>
+            <div class="divider"></div>
+            <div class="lest-talk-container">
+                <div class="lest-talk-title">Have a project in mind ?</div>
+                <div class="lest-talk-btn show-contact-btn">let’s Talk</div>
+            </div>
+        </div>
+    </div>
+    <div class="section" id="our-clients">
+        <div class="section-header">
+            <div class="spacial-title">
+                <div class="spacial-title-bg"></div>
+                <div class="spacial-title-text"></div>
+            </div>
+            <div class="section-description">
+                Yes!!
+            </div>
+        </div>
+    </div>
+    <div id="logo"></div>
+    <div id="back"></div>
+    <div id="contact">
+        <div class="contact-toggle-btn noselect">contact</div>
+        <div class="contact-body">
+            <div class="contact-container">
+                <div class="contact-form">
+                    <input type="text" placeholder="Name">
+                    <input type="text" placeholder="E-Mail">
+                    <input type="text" placeholder="Subject">
+                    <textarea placeholder="Message"></textarea>
+                </div>
+                <div class="contact-info">
+                    <h4>Head office :</h4>
+                    <p>1991/192 ถนนอ่อนนุช แขวงสวนหลวง เขตสวนหลวง กรุงเทพ 10250</p>
+                    <h4>Phone :</h4>
+                    <p>(66)-2101-8972 , (66)-83-388-4388</p>
+                    <h4>Email :</h4>
+                    <p>info@tufftex.co.th</p>
+                    <h4>Socail Network :</h4>
+                    <div class="social-list">
+                        <a href="#"><i class="social-icon social-icon-facebook"></i></a>
+                        <a href="#"><i class="social-icon social-icon-twitter"></i></a>
+                        <a href="#"><i class="social-icon social-icon-google_plus"></i></a>
+                        <a href="#"><i class="social-icon social-icon-rss"></i></a>
+                        <a href="#"><i class="social-icon social-icon-pinterest"></i></a>
+                    </div>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+var Page = (function(){
+
+    var isActive = false;
+    var sectionActive = null;
+
+    var windowH;
+
+    var $back;
+    var $sections;
+    var $activeEl;
+    var $logo;
+
+    function _page(){}
+    _page.prototype.refreshRender = function(){
+        if(isActive){
+            $back.addClass('active');
+            $sections.addClass('inactive');
+            $activeEl.removeClass('inactive').addClass('active');
+            switch($activeEl.attr('id')) {
+                case 'about':
+                    $logo.css({top: '120%', left: '120%'});
+                    break;
+                case 'media':
+                    $logo.css({top: '120%', left: '-20%'});
+                    break;
+                case 'investor':
+                    $logo.css({top: '-20%', left: '120%'});
+                    break;
+                case 'our-clients':
+                    $logo.css({top: '-20%', left: '-20%'});
+                    break;
+            }
+        }
+        else {
+            $back.removeClass('active');
+            $activeEl.removeClass('active');
+            $sections.removeClass('inactive');
+            $logo.css({top: '', left: ''});
+        }
+    };
+
+    _page.prototype.showContact = function(){
+        $('#contact').addClass('active');
+    };
+
+    _page.prototype.setupParallax1 = function(){
+        var $wrap = $("#investor");
+        var $parallax = $('.parallax1');
+        var h = windowH + $parallax.height();
+        $wrap.scroll(function(){
+            if($parallax.offset().top < windowH && $parallax.offset().top + $parallax.height() > 0){
+                var pos = windowH - $parallax.offset().top;
+                var perc = (pos/h)*100;
+                console.log("in", perc);
+                $parallax.find('.parallax-bg').css('background-position', '0 '+perc+'%');
+            }
+        });
+    };
+
+    _page.prototype.run = function(){
+        $back = $('#back');
+        $logo = $('#logo');
+        $sections = $('.section');
+        windowH = $(window).height();
+
+        var that = this;
+        $('.spacial-title').click(function(e){
+            e.preventDefault();
+            if(isActive) return;
+
+            $activeEl = $(this).closest('.section');
+            sectionActive = $activeEl.attr('id');
+            isActive = true;
+            that.refreshRender();
+        });
+
+        $back.click(function(e){
+            e.preventDefault();
+            sectionActive = null;
+            isActive = false;
+
+            that.refreshRender();
+        });
+
+        $('.contact-toggle-btn').click(function(e){
+            e.preventDefault();
+            $('#contact').toggleClass('active');
+        });
+
+        $('.show-contact-btn').click(function(e){
+            e.preventDefault();
+            that.showContact();
+        });
+
+        that.setupParallax1();
+        $(window).resize(function(){
+            windowH = $(window).height();
+        });
+    };
+
+    return _page;
+})();
+
+$(function(){
+    var page = new Page();
+    page.run();
+});
+</script>
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-37675183-3', 'auto');
+    ga('send', 'pageview');
+
+</script>
+</body>
+</html>
